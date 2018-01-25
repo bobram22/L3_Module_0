19b281ae60d6ab2080deb5e0d7185558cb17f3af
League <league@Mac-mini.local>
2018-01-24 18:02:02 -0800

    Learned ArrrayList

--- src/IntroToArrayLists/IntroToArrayLists.java
+++ src/IntroToArrayLists/IntroToArrayLists.java
@@ -1,14 +1,22 @@
 package IntroToArrayLists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-		
+		ArrayList<String> People = new ArrayList<String>();
 		//2. Add five Strings to your list
-		
+		People.add("Coach");
+		People.add("Jack");
+		People.add("Nancy");
+		People.add("Roberto");
+		People.add("Denis");
 		//3. Print all the Strings using a standard for-loop
-		
+		for(int i = 0; i < People.size(); i++) {
+			System.out.println(People.get(i));
+		}
 		//4. Print all the Strings using a for-each loop
 		
 		//5. Print only the even numbered elements in the list.
