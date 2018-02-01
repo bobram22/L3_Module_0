fc98ca4e0474ae9ee9819b008cf86bbffd33d0e7
tactreconx <league@Admins-Mac-mini.local>
2018-01-31 17:50:42 -0800

    Finished ArrayList
    Started the first array puzzle

---
+++ src/IntroToArrayLists/DinnerDecider.java
@@ -0,0 +1,10 @@
+package IntroToArrayLists;
+
+public class DinnerDecider {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/IntroToArrayLists/IntroToArrayLists.java
+++ src/IntroToArrayLists/IntroToArrayLists.java
@@ -4,25 +4,42 @@ import java.util.ArrayList;
 
 public class IntroToArrayLists {
 	public static void main(String[] args) {
-		//1. Create an array list of Strings
-		//   Don't forget to import the ArrayList class
+		// 1. Create an array list of Strings
+		// Don't forget to import the ArrayList class
 		ArrayList<String> People = new ArrayList<String>();
-		//2. Add five Strings to your list
+		// 2. Add five Strings to your list
 		People.add("Coach");
 		People.add("Jack");
 		People.add("Nancy");
 		People.add("Roberto");
 		People.add("Denis");
-		//3. Print all the Strings using a standard for-loop
-		for(int i = 0; i < People.size(); i++) {
+		// 3. Print all the Strings using a standard for-loop
+		for (int i = 0; i < People.size(); i++) {
 			System.out.println(People.get(i));
 		}
-		//4. Print all the Strings using a for-each loop
-		for(String)
-		//5. Print only the even numbered elements in the list.
-		
-		//6. Print all the Strings in reverse order.
-		
-		//7. Print only the Strings that have the letter 'e' in them.
+		System.out.println("");
+		// 4. Print all the Strings using a for-each loop
+		for (String s : People) {
+			System.out.println(s);
+		}
+		System.out.println("");
+		// 5. Print only the even numbered elements in the list.
+		for (int i = 0; i < People.size(); i++) {
+			if (i % 2 == 0) {
+				System.out.println(People.get(i));
+			}
+		}
+		System.out.println("");
+		// 6. Print all the Strings in reverse order.
+		for (int i = People.size() - 1; i >= 0; i--) {
+			System.out.println(People.get(i));
+		}
+		// 7. Print only the Strings that have the letter 'e' in them.
+		System.out.println("");
+		for (String s : People) {
+			if (s.contains("e")) {
+				System.out.println(s);
+			}
+		}
 	}
 }
